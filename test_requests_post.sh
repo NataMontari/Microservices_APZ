4924769c84cb569949c8e38120524697c11f737e
NataMontari <sydor.pn@ucu.edu.ua>
2025-02-16 22:59:21 +0200

    images

--- test_requests_post.sh
+++ test_requests_post.sh
@@ -2,4 +2,4 @@
 
 # Send POST request
 echo -e "\nSending POST request..."
-curl -X POST http://localhost:8080/send_message -H "Content-Type: application/json" -d '{"message": "msg3"}'
+curl -X POST http://localhost:8080/send_message -H "Content-Type: application/json" -d '{"message": "different message"}'
